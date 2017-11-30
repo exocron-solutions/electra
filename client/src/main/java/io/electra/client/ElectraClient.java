8f8fd0bf1a8b45cbb0cb206a39476391310c47bd
JackWhite20 <phil.westf@web.de>
2017-11-30 19:28:20 +0100

    Add consumers to the get methods.

--- client/src/main/java/io/electra/client/ElectraClient.java
+++ client/src/main/java/io/electra/client/ElectraClient.java
@@ -1,13 +1,15 @@
 package io.electra.client;
 
+import java.util.function.Consumer;
+
 /**
  * @author Philip 'JackWhite20' <silencephil@gmail.com>
  */
 public interface ElectraClient {
 
-    void get(String key);
+    void get(String key, Consumer<String> consumer);
 
-    void get(byte[] keyBytes);
+    void get(byte[] keyBytes, Consumer<byte[]> consumer);
 
     void put(String key, String value);
 
