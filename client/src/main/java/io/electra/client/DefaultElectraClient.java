0cbe5af52d9b79a216fbcc821509016a40071673
Philip <phil.westf@web.de>
2017-12-14 00:22:26 +0100

    Feature/client (#61)
    * Start client experimenting.
    
    * Use pipeline utils to automatically select NIO or epoll server channel class.
    
    * Use pipeline utils to automatically select NIO or epoll channel class.

--- client/src/main/java/io/electra/client/DefaultElectraClient.java
+++ client/src/main/java/io/electra/client/DefaultElectraClient.java
@@ -8,7 +8,6 @@ import io.netty.bootstrap.Bootstrap;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.Channel;
-import io.netty.channel.socket.nio.NioSocketChannel;
 import org.apache.commons.codec.Charsets;
 
 import java.util.Arrays;
@@ -38,7 +37,7 @@ public class DefaultElectraClient implements ElectraClient {
         try {
             Bootstrap bootstrap = new Bootstrap()
                     .group(PipelineUtils.newEventLoopGroup(2, new ElectraThreadFactory("Electra Client Thread")))
-                    .channel(NioSocketChannel.class)
+                    .channel(PipelineUtils.getChannel())
                     .handler(new ElectraChannelInitializer(electraBinaryHandler = new ElectraBinaryHandler()));
 
             channel = bootstrap.connect(host, port).sync().channel();
